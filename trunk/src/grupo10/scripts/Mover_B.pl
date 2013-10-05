3c67f162e2e49a4b235056a1c52ffab63a05dc6a
federicomrossi@gmail.com <federicomrossi@gmail.com@d3119b63-f918-43b3-dd37-c07f5e4307df>
2013-10-05 21:47:08 +0000

    Mover_B: agregada la grabación virtual en el log. Falta modificar para que se grabe realmente

--- trunk/src/grupo10/scripts/Mover_B.pl
+++ trunk/src/grupo10/scripts/Mover_B.pl
@@ -14,6 +14,7 @@
 # versión menos reciente del archivo hacia allí y así realizar posteriormente
 # el movimiento del archivo nuevo.
 #
+#
 # Parámetros
 # ##########
 #
@@ -21,7 +22,9 @@
 # 							 del archivo a mover. (ej: /origen/mi_archivo.txt).
 # Parametro 2 (Obligatorio): Path destino hacia donde debe moverse el archivo. 
 #							 (ej; destino/).
-# 
+# Parametro 3 (Opcional): Nombre del comando que invoca al Mover_B.
+#
+#
 # Códigos de retorno
 # ##################
 #
@@ -39,13 +42,64 @@ require 'libs/lib_utilities.pl';
 
 
 
+# ################# Subrutinas auxiliares ################# #
+
+# Subrutina que se encarga de evaluar si debe ingresarse como entrada del log
+# el resultado de la operación
+# PRE: Recibe el nombre del comando, el origen, el destino y el código que 
+# especifica el resultado de la operación (0 en caso de éxito; 1 en caso de 
+# error).
+sub grabarSiCorresponde
+{
+	$comando = $_[0];
+	$origen = $_[1];
+	$destino = $_[2];
+	$resOperacion = $_[3];
+
+	@comandosDeLog = {"Recibir_B", "Reservar_B"};
+
+	for($i = 0; $i < (scalar @comandosDeLog); $i++)
+	{
+		if($comando eq $comandosDeLog[$i])
+		{
+			# Caso de error en el movimiento de mover
+			if($resOperacion == 1)
+			{
+				# REEMPLAZAR
+				print "COMANDO: $comando, MENSAJE: Error al intentar mover. El directorio origen ($origen) no existe.";
+				# END REEMPLAZAR
+				return;
+			}
+			elsif($resOperacion == 2)
+			{
+				# REEMPLAZAR
+				print "COMANDO: $comando, MENSAJE: Error al intentar mover. El directorio destino ($destino) no existe.";
+				# END REEMPLAZAR
+				return;
+			}
+
+			# Caso de exito en el movimiento de mover
+			# REEMPLAZAR
+			print "COMANDO: $comando, MENSAJE: Se ha movido desde el origen $origen hacia el directorio destino $destino";
+			# END REEMPLAZAR
+		}
+	}
+}
+
+
+
+
+
+# ######################################################### #
+# ################### PROGRAMA PRINCIPAL ################## #
+# ######################################################### #
 
 # Almacenamos parámetros de entrada
 $origen = $ARGV[0];
 $destino = $ARGV[1];
-$comando;
+$comando = "";
 
-# Comprobamos si existe un tercer parametro con el nombre del comando invocante.
+# Comprobamos si existe un tercer parametro con el nombre del comando invocante
 # En el transcurso del programa, se utiliza para grabar en el log si el comando
 # invocante lo hace.
 if ($ARGV[2]) {
@@ -57,21 +111,21 @@ $dirDuplicados = $destino . "/dup/";
 
 
 # Verificamos si el origen existe
-open($origen, $origen) or (grabarSiCorresponde($comando, 1) and exit 1);
+open($origen, $origen) or (grabarSiCorresponde($comando, $origen, $destino, 1) and exit 1);
 close $origen;
 
 # Verificamos si el directorio destino existe. De ser así, lo dejamos abierto.
-opendir($destino, $destino) or (grabarSiCorresponde($comando, 2) and exit 2);
+opendir($destino, $destino) or (grabarSiCorresponde($comando, $origen, $destino, 2) and exit 2);
 # Cerramos el directorio
 closedir $destino;
 
 # Verificamos si el origen es igual al destino en cuyo caso ya se movió el
 # archivo
 ($pathOrigen, $nombreArchivo) = obtenerDir($origen);
-(grabarSiCorresponde($comando, 0) and exit 0) if($pathOrigen eq $destino);
+(grabarSiCorresponde($comando, $origen, $destino, 0) and exit 0) if($pathOrigen eq $destino);
 
 # Verificamos la existencia de archivos duplicados
-opendir($destino, $destino) or (grabarSiCorresponde($comando, 2) and exit 2);
+opendir($destino, $destino) or (grabarSiCorresponde($comando, $origen, $destino, 2) and exit 2);
 
 # Buscamos si algún archivo posee el mismo nombre.
 foreach(grep(/^($nombreArchivo)$/, readdir($destino)))
@@ -104,6 +158,6 @@ foreach(grep(/^($nombreArchivo)$/, readdir($destino)))
 $error = move $origen, $destino;
 
 # Devolvemos el codigo de error
-grabarSiCorresponde($comando, $error);
+grabarSiCorresponde($comando, $origen, $destino, $error);
 
 closedir $destino;
