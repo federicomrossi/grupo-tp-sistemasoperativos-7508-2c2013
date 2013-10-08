1f2c38e94f783989c7ca45c8cd10405573a17ade
belubeltran@gmail.com <belubeltran@gmail.com@d3119b63-f918-43b3-dd37-c07f5e4307df>
2013-10-08 00:35:07 +0000

    Reservar_B comprueba correctamente si se trata de una fecha valida

--- trunk/src/grupo10/scripts/debug/Reservar_B.sh
+++ trunk/src/grupo10/scripts/debug/Reservar_B.sh
@@ -70,6 +70,35 @@ function validarFecha() {
 	fi
 }
 
+function verificarAnticipacion() {
+	local fecha_funcion=$1
+	local hoy=`date +%D`
+
+	# Invierto la fecha para obtener el formato mm/dd/aaaa
+	fecha_funcion=`echo "${fecha_funcion}" | sed 's:\([^/]\+\)/\([^/]\+\)/\([^/]\+\):\2/\1/\3:'`
+
+	# Obtengo distancia entre fechas en funcion de 'dias'
+	distancia_dias=$(( ((`date --date="$fecha_funcion" +%s`) - (`date --date="$hoy" +%s`)) / (60*60*24) ))
+	
+	# Si la reserva es para el dia actual o para el dia siguiente, la rechazo
+	if [ "$distancia_dias" -lt "2" ]; then
+		if [ "$distancia_dias" -ge "0" ]; then
+			echo "Termino la entrega querido, mas rapido la proxima vez"
+		# Si es para una fecha vencida, la rechazo
+		elif [ "$distancia_dias" -lt "0" ]; then
+			echo "La fecha de la funcion esta mas vencida que mi dulce de leche"
+		fi
+	else
+		# Si la reserva tiene mas de 30 dias de anticipacion, la rechazo
+		if [ "$distancia_dias" -gt "30" ]; then
+			echo "Tas apurado que pedis con tanta anticipacion?"
+		else
+			# Es para una fecha valida
+			echo "U r good 2 go!!!!!"
+		fi
+	fi
+}
+
 
 # DEBUG: Se debe cambiar el path ingresado por la variable de entorno:
 
@@ -106,35 +135,40 @@ for j in `seq 1 ${cant_archivos}`; do
 		fecha=`echo ${linea} | cut -d ';' -f "2"`
 
 		# Se valida la fecha
-		validarFecha $fecha
+		fecha_valida=`validarFecha $fecha`
+
+		if [ -n "$fecha_valida" ]; then
+			# Se comprueba si se trata de una fecha vencida o muy anticipada
+			verificarAnticipacion $fecha
+		fi
 
 	done
 done
 
-FILES=../*
-# Iteramos sobre los archivos del directorio
-for f in $FILES
-do
-        # Procesamos solo los que son archivos
-        if [ -f $f ]; then
+# FILES=../*
+# # Iteramos sobre los archivos del directorio
+# for f in $FILES
+# do
+#         # Procesamos solo los que son archivos
+#         if [ -f $f ]; then
 
-                # Verificamos si ya ha sido procesado el archivo anteriormente
-                `ls -l -lp Mover_B.pl &> /dev/null`
-                res=$?
+#                 # Verificamos si ya ha sido procesado el archivo anteriormente
+#                 `ls -l -lp Mover_B.pl &> /dev/null`
+#                 res=$?
 
-                # Si se encontró el archivo, está duplicado
-                if [ "$res" -eq "0" ]; then
+#                 # Si se encontró el archivo, está duplicado
+#                 if [ "$res" -eq "0" ]; then
 
-                        # Movemos archivo a $RECHDIR
-                        'perl ./Mover_B.pl $f $RECHDIR Reservar_B'
+#                         # Movemos archivo a $RECHDIR
+#                         'perl ./Mover_B.pl $f $RECHDIR Reservar_B'
 
-                        # Generamos entrada nueva en el log informando la situación
-                        echo "todo piola"
+#                         # Generamos entrada nueva en el log informando la situación
+#                         echo "todo piola"
 
-                # Caso en que no se encontró el archivo duplicado        
-                else
-                        # Movemos archivo a $PROCDIR
-                        'perl ./Mover_B.pl $f $PROCDIR Reservar_B'
-                fi
-        fi
-done
+#                 # Caso en que no se encontró el archivo duplicado        
+#                 else
+#                         # Movemos archivo a $PROCDIR
+#                         'perl ./Mover_B.pl $f $PROCDIR Reservar_B'
+#                 fi
+#         fi
+# done
