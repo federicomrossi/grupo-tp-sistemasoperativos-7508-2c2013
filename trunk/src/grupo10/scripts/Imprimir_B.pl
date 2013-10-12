943a0556c84b91c3c04f289a1abf30d08fc8fc19
federicomrossi@gmail.com <federicomrossi@gmail.com@d3119b63-f918-43b3-dd37-c07f5e4307df>
2013-10-12 18:12:57 +0000

    Imprimir_B: subida del archivo

---
+++ trunk/src/grupo10/scripts/Imprimir_B.pl
@@ -0,0 +1,171 @@
+#!/usr/bin/perl
+use Getopt::Std;
+
+
+################################## HARDCODEO
+
+$PROCDIR = "../procesados/";
+$RESERVASOK = "reservas.ok";
+
+################################## END HARDCODEO
+
+
+
+# Variable que contiene el display de la ayuda
+$ayuda = "\n** Ayuda Imprimir_B **\n
+Uso: Imprimir_B [OPCION]
+o: Imprimir_B -w [OPCION]
+
+Imprime en pantalla o graba en archivo (utlilzando -w) la informacion 
+correspondiente a OPCION.
+
+Nota: Todos las opciones pueden ser combinadas con -w, salvo en caso
+de que sea especificado lo contrario.
+
+OPCION:
+  -a 			Muestra esta ayuda. No puede ser combinado con -w.
+  -d 			Genera una lista de disponibilidades para una
+  				obra o una sala.
+  -i 			Genera una lista de invitados a un evento.
+  -r 			Genera un ranking con los 10 principales 
+  				solicitantes de reservas.
+  -t 			Genera un listado de tickets a imprimir.
+
+Para ver la documentacion completa, correr: ..\n
+** Fin Ayuda Imprimir_B **\n\n";
+
+# Variable que contiene el mensaje de error para el caso en que no se han
+# especificado parámetros de entrada.
+$errorCantidadNulaDeParametros = "-> No se han especificado parámetros de entrada. Para mas información ejecute
+Imprimir_B.pl -a\n";
+
+# Variable que contiene el mensaje de error de combinacion de opciones
+$errorCombinacionOpciones = "
+-> Ha ingresado una combinacion de opciones incorrecta. Por favor, verifique que el comando ingresado 
+   sea correcto y vuelva a intentarlo.
+-> Para ver la ayuda, ingrese: perl Imprimir_B -a.\n\n";
+
+# Variable que contiene el mensaje de error de repeticion de opciones
+$errorRepeticion = "
+-> Ha ingresado una o varias opciones repetidas. Por favor, verifique que el comando ingresado sea correcto y vuelva a intentarlo.
+-> Para ver la ayuda, ingrese: perl Imprimir_B -a.\n\n";
+
+
+sub disponibilidad{
+
+	# $string = "The time is: 12:31:02 on 4/12/00";
+	# print "$string\n";
+	# $string =~ /:\s+/g;
+	# ($time) = ($string =~ /\G(\d+:\d+:\d+)/);
+	# $string =~ /.+\s+/g;
+	# ($date) = ($string =~ m{\G(\d+/\d+/\d+)});
+	# print "Time: $time, Date: $date\n";
+
+	# Abre el archivo de combos.dis
+	open(COMBOS, "<combos.dis") or die "Couldn't open file combos.dis, $!";
+
+	# Lee una linea
+	$linea = <COMBOS>;
+
+	while ($linea != null) {
+		# Compara con el formato buscado, si es, lo imprime
+		if ($linea =~ m[(\d+;2;(\d+)\/(\d+)\/(\d+);(\d+):(\d+);\d+;\d+;\d+);\w+])	{
+			$linea = $1;
+			$linea =~ s/;/-/g;
+			print "$linea\n";
+		}
+		# Lee una linea
+		$linea = <COMBOS>;
+	}
+}
+
+
+# [ Insertar documentación aquí ]
+sub rankingDeSolicitantes {
+
+	# Hash con solicitantes mas rankeados
+	%rank = ();
+
+
+	open FILE, "$PROCDIR$RESERVASOK" or die $!;
+	my $lineno = 1;
+	
+	while (<FILE>) {
+		print $lineno++;
+		print ": $_";
+	}
+
+}
+
+
+
+# ############################# #
+# MAIN							#
+# ############################# #
+
+
+# Se obtiene la cantidad inicial de argumentos
+my $cantArg = scalar @ARGV;
+
+if($cantArg == 0) {
+	print "$errorCantidadNulaDeParametros";
+	exit 1;
+}
+
+# Se obtienen las opciones insertadas en linea de comandos
+$ok = getopts('awidrt', \%Opciones);
+
+if($ok) {
+
+	# Se obtiene el tamanio del hash
+	@claves = keys %Opciones;
+	$tamanio = @claves;
+
+	# Si hay mas de 2 opciones, se ingresaron mal los parametros
+	if ($tamanio > 2) {
+		print "$errorCombinacionOpciones";
+
+	}
+
+	# Si se repite alguna opcion, tambien hay un error
+	elsif (($tamanio == 1 && $cantArg > 1) || ($tamanio == 2 && $cantArg > 2))
+	{
+		print "$errorRepeticion";	
+	}
+
+	# Si hay exactamente 2 opciones, se busca que exista -w.
+	# Si hay una opcion, se busca que no exista ni -w ni -a
+	elsif (($tamanio == 2 && exists $Opciones{'w'}) || ($tamanio == 1 && !exists $Opciones{'w'} && !exists $Opciones{'a'})) {
+		if (exists $Opciones{'i'}) {
+			print "Elegi i\n"; 
+		}
+		elsif (exists $Opciones{'d'}) {
+			print "Elegi d \n"; 
+			disponibilidad();
+		}
+		elsif (exists $Opciones{'r'}) {
+			print "Elegi r \n"; 
+		}
+		elsif (exists $Opciones{'t'}) {
+			print "Elegi t \n"; 
+		}
+		else {
+			# Se ingreso la opcion -w en conjunto con la opcion -a
+			print "$errorCombinacionOpciones";				
+		}
+	}
+
+	# Si el tamanio es 1 y pidieron la ayuda
+	elsif ($tamanio == 1 && exists $Opciones{'a'}) {
+		#print $ayuda; 
+		print `cat Ayuda`;
+	}
+
+	# Se ingresaron otra lista de opciones incorrectas
+	else {
+		print "$errorCombinacionOpciones";	
+	}
+}
+else {
+	print "$errorCombinacionOpciones";
+}
